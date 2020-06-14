df0d87da98dad8a52a17211ffc30ea3a2b5b06ee
SuperTux20 <SuperTux20@users.noreply.github.com>
2020-06-13 21:59:15 -0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 # bot.py
 import os
 import random
+import discord
 from discord.ext import commands
 from dotenv import load_dotenv
 
@@ -17,13 +18,17 @@ async def say_hi_back(ctx):
     await ctx.send(response)
 
 @bot.command(name="bye", help="You say bye, I bid you farewell.")
-async def say_hi_back(ctx):
+async def say_bye_back(ctx):
     responses = ["See you next time!", "Bye!", "So long, gay bowser!"]
     response = random.choice(responses)
     await ctx.send(response)
 
+@bot.command(name="repeat", help="You say something, I repeat it.")
+async def say_back(ctx, *, arg):
+    await ctx.send(arg)
+
 @bot.command(name="roll", help="Roll some dice! Be sure to specify the number of dice, then the number of sides they have (all dice have the same number of sides).\nThe currently known limits are...\n\t- 658 dice with 1 side\n\t- 657 dice with 2-9 sides\n\t- 616 dice with 10 sides")
-async def rollem(ctx, dice, sides):
+async def rollem(ctx, dice: int, sides: int):
     if dice == "0":
         await ctx.send("Hey, you can't roll zero dice!")
     elif sides == "0":
@@ -41,15 +46,23 @@ async def rollem(ctx, dice, sides):
     else:
         sideList = []
         rollResults = []
-        for side in range(int(sides)):
+        for side in range(sides):
             sideList.append(side + 1)
-        for result in range(int(dice)):
+        for result in range(dice):
             rollResults.append(sideList[random.randint(0, sideList[-1])-1])
         total = sum(rollResults)
-        if int(dice) > 1:
+        if dice > 1:
             response = "You rolled `" + str(rollResults) + "`, totalling " + str(total)
         else:
             response = "You rolled " + str(total)
         await ctx.send(response)
 
+@bot.command()
+async def slap(ctx, members: commands.Greedy[discord.Member], *, reason="no reason"):
+    slapped = ", ".join(x.name for x in members)
+    await ctx.send(members)
+    if slapped == " ":
+        await ctx.send("{0} just got slapped by {1.author} for {2}".format(slapped, ctx, reason))
+    else:
+        await ctx.send("You can't just slap thin air!")
 bot.run(TOKEN)
