f4d0f900eb220d305d0d9bae0bae3b1337aac456
SuperTux20 <SuperTux20@users.noreply.github.com>
2020-07-02 07:07:27 -0700

    add greekifier

--- cogs/converters.py
+++ cogs/converters.py
@@ -14,7 +14,7 @@ class Converters(commands.Cog):
                 outputString = outputString.replace(vowels[vowel],"")
         await ctx.send(outputString.replace("  ", " ")) # fix doubled spaces
 
-    @commands.command(name="owo", help="Convert whatever text into owo-speak... oh god why did i make this", aliases=["furry"])
+    @commands.command(name="owo", help="Convert whatever text into owo-speak... oh god why did i make this", aliases=["uwu", "furry"])
     async def owoConverter(self, ctx, *, arg):
         await ctx.send(arg.replace("l","w").replace("r","w") + " " + choice(["OwO","UwU","owo","uwu","ewe","O3O","U3U","o3o","u3u","^w^","nya~","rawr"]))
         await ctx.message.delete()
@@ -38,6 +38,17 @@ class Converters(commands.Cog):
         for beeg in range(len(textlist)):
             finaltext = finaltext + textlist[beeg]
         await ctx.send(finaltext)
+
+    @commands.command(name="greekify", help="Make words *look* Greek, but the pronunciation is still almost the same as in English.")
+    async def greekify(self, ctx, *, arg):
+        alphabet = ["CH","PS","AV","AF","EV","EF","OO","EH","TH","YE","YI","YU","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W", "X","Y","Z"]
+        greekphabet = ["Χ","Ψ","ΑΥ","ΑΥ","ΕΥ","ΕΥ","ΟΥ","ΑΙ","Θ","Γ", "Γ", "Γ", "Α","Β","K","Δ","Ε","Φ","Γ","", "Ι","Γ","Κ","Λ","Μ","Ν","Ο","Π","Κ","Ρ","Σ","Τ","Ω","Φ","ΟΥ","Ξ","Υ","Ζ"]
+        alphabet = alphabet + [letter.lower() for letter in alphabet]
+        greekphabet = greekphabet + [letter.lower() for letter in greekphabet]
+        toconvert = arg
+        for letter in range(len(alphabet)):
+            toconvert = toconvert.replace(alphabet[letter], greekphabet[letter])
+        await ctx.send(toconvert)
 
 def setup(client):
     client.add_cog(Converters(client))
