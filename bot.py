fe91d8bc36db57fbbf8d5a956166f0e0263949d7
Tux Penguin <tuxpengwin@gmail.com>
2021-04-09 15:48:28 -0700

    update bot.py

--- bot.py
+++ bot.py
@@ -416,37 +416,40 @@ if not unstable:
         await ctx.send(f"An error occured while updating...```{error}```Attempting force-update.")
         await forceupdate(ctx)
 
-    @client.command(name="updatelog", aliases=["ul"])
-    async def update(ctx, formatted=True):
-        if developer(ctx.author):
-            update_log = [line.replace("\n","") for line in open("update.log", "r")][1:]
-            if "A" == update_log[0][0]:
-                await ctx.send(content=f'```json\n"a{update_log[0][1:-1]}"```')
-            elif formatted:#
-                update_summary = update_log[-1]
-                update_log = update_log[2:-1]
-                if options(ctx.guild.id, "jsonMenus"):
-                    update_summary = update_summary.split(", ")
-                    await ctx.send(str(update_summary))
-                    update_summary = {{"files changed": update_summary[0][1:].split()[0]}: {"insertions": update_summary[1][:-3].split()[0], "deletions": update_summary[2][:-3].split()[0]}}
-                    await ctx.send(str(update_summary))
-                    for item in range(len(update_log)):
-                        while "  " in update_log[item]:
-                            update_log[item] = update_log[item].replace("  ", " ")
-                        await ctx.send(update_log[item])
-                    await ctx.send(update_log)
-                    update_log = {
-                        update_log[item].split("|")[0].replace(" ", ""): update_log[item].split("|")[1][1:]
-                        for item in range(len(update_log))
+@client.command(name="updatelog", aliases=["ul"])
+async def update(ctx, formatted=True):
+    if developer(ctx.author):
+        update_log = [line.replace("\n","") for line in open("update.log", "r")][1:]
+        if "A" == update_log[0][0]:
+            await ctx.send(content=f'```json\n"a{update_log[0][1:-1]}"```')
+        elif formatted:
+            update_summary = update_log[-1]
+            update_log = update_log[2:-1]
+            if options(ctx.guild.id, "jsonMenus"):
+                update_summary = update_summary.split(", ")
+                update_summary = [
+                    {
+                        "files changed": int(update_summary[0][1:].split()[0])
+                    },
+                    {
+                        "insertions": int(update_summary[1][:-3].split()[0]),
+                        "deletions": int(update_summary[2][:-3].split()[0])
                     }
-                    await ctx.send(str(update_log))
-                    await ctx.send(f'```json\n"summary": {dumps(update_summary, indent=4)},\n"changes": {dumps(update_log, indent=4)}```')
-                else:
-                    update_log = update_log[:-1]
-                    await ctx.send(embed=discord.Embed(title="Updating...", description=list2str(update_log, 3), color=32639).set_footer(text=update_summary))
-            await ctx.send(f'Raw log contents...```{open("update.log", "r").read()}```')
-        else:
-            await ctx.send("Hey, only my developers can do this!")
+                ]
+                for item in range(len(update_log)):
+                    while "  " in update_log[item]:
+                        update_log[item] = update_log[item].replace("  ", " ")
+                update_log = {
+                    update_log[item].split("|")[0].replace(" ", ""): update_log[item].split("|")[1][1:]
+                    for item in range(len(update_log))
+                }
+                await ctx.send(f'```json\n"summary": {dumps(update_summary, indent=4)},\n"changes": {dumps(update_log, indent=4)}```')
+            else:
+                update_log = update_log[:-1]
+                await ctx.send(embed=discord.Embed(title="Updating...", description=list2str(update_log, 3), color=32639).set_footer(text=update_summary))
+        await ctx.send(f'Raw log contents...```{open("update.log", "r").read()}```')
+    else:
+        await ctx.send("Hey, only my developers can do this!")
 
 @client.group(name="help", help="Show this message", aliases=["commands", "h", "?"])
 async def help(ctx, *, cogname: str = None):
