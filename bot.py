66b3a860ad458c8ca7c469e439ec92815d739503
Tux Penguin <tuxpengwin@gmail.com>
2021-04-09 07:01:52 -0700

    woooooooo!

--- bot.py
+++ bot.py
@@ -372,7 +372,6 @@ if not unstable:
             await status.edit(content="Pulling the latest commits from GitHub...")
             os.system("bash update.sh > update.log")
             update_log = [line for line in open("update.log", "r")][3:]
-            await ctx.send(update_log)
             if "Already up to date.\n" in update_log:
                 await status.edit(content="Already up to date, no restart required.")
                 await status_switcher()
