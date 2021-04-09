6a53295435ec138f504c24843d032ea675124479
Tux Penguin <tuxpengwin@gmail.com>
2021-04-09 14:53:10 -0700

    update bot.py

--- bot.py
+++ bot.py
@@ -417,9 +417,10 @@ if not unstable:
             if "Already up to date.\n" in update_log:
                 await ctx.send(content='```json\n"Already up to date."```')
             else:
-                update_summary = update_log[2:-1]
+                update_log = update_log[2:]
+                update_summary = update_log[-1]
                 await ctx.send(update_summary)
-                update_log = update_log[2:-1]
+                await ctx.send(update_log)
                 if options(ctx.guild.id, "jsonMenus"):
                     update_summary = update_summary.split(", ")
                     update_summary = {update_summary[0]: [update_summary[1][:-3], update_summary[2][:-3]]}
