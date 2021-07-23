53755667e8922409f7e6773be91f137b4c0237fd
SuperTux20 <tuxpengwin@gmail.com>
2021-07-23 15:21:58 -0700

    HANGMAN!

--- cogs/games.py
+++ cogs/games.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3.9
 # -*- coding: utf-8 -*-
 
+import discord
 from discord.ext import commands
 from random import choice, randint
+from collections import Counter
+from pengaelicutils import hangman_words, regional_indicators
 
 
 class Games(commands.Cog):
@@ -330,11 +333,93 @@ class Games(commands.Cog):
             )
         await ctx.send(sheet)
 
+    @commands.command(
+        name="hangman",
+        help="A classic! Guess the letters to solve the word before you run out of attempts.",
+    )
+    async def hangman(self, ctx):
+        word = choice(list(hangman_words.keys()))
+        vowels = 0
+        for i in word:
+            if i in ["a", "e", "i", "o", "u", "y"]:
+                vowels += 1
+        await ctx.send(
+            f"React to this message and guess the word.\nHINT: The word has {vowels} vowels."
+        )
+        the_word = await ctx.send(" ".join(list("_" * len(word))).replace("_", "\_"))
+        # list for storing the letters guessed by the player
+        past_guesses = ""
+        letter_guessed = ""
+        chances = 6
+        correct = 0
+        flag = False
+        chance_counter = await ctx.send(f"{chances} chances remaining.")
+        while (
+            chances != 0
+        ) and flag == False:  # flag is updated when the word is correctly guessed
+
+            def check(reaction, user):
+                return user == ctx.author and reaction.emoji in list(
+                    regional_indicators.keys()
+                )
+
+            reaction, user = await self.client.wait_for("reaction_add", check=check)
+            if user:
+                guess = regional_indicators[reaction.emoji]
+            # Validation of the guess
+            if not guess.isalpha():
+                await ctx.send("Enter only a letter!")
+            elif len(guess) > 1:
+                await ctx.send("Enter only a SINGLE letter")
+            elif guess in letter_guessed or guess in past_guesses:
+                await ctx.send("You have already guessed that letter")
+            elif guess in word:  # If letter is guessed correctly
+                k = word.count(
+                    guess
+                )  # k stores the number of times the guessed letter occurs in the word
+                for _ in range(k):
+                    letter_guessed += (
+                        guess  # The guess letter is added as many times as it occurs
+                    )
+            else:  # if letter is guessed incorrectly
+                chances -= 1
+                past_guesses += guess
+                await chance_counter.edit(content=f"{chances} chances remaining.")
+            # Print the word
+            word_to_print = ""
+            for char in word:
+                if char in letter_guessed and (
+                    Counter(letter_guessed) != Counter(word)
+                ):
+                    word_to_print += char
+                    correct += 1
+                # If user has guessed all the letters
+                elif Counter(letter_guessed) == Counter(
+                    word
+                ):  # Once the correct word is guessed fully,
+                    # the game ends, even if chances remain
+                    await chance_counter.edit(
+                        content="Congratulations, you won!\n" + hangman_words[word]
+                    )
+                    await the_word.edit(content=" ".join(list(word)))
+                    flag = True
+                    break
+                else:
+                    word_to_print += "_"
+            await the_word.edit(
+                content=" ".join(list(word_to_print)).replace("_", "\_")
+            )
+
+        # If user has used all of his chances
+        if chances <= 0 and (Counter(letter_guessed) != Counter(word)):
+            await chance_counter.edit(content=f'You lost!\nThe word was "{word}".')
+
     @magic_8_ball.error
     @roll_dice.error
     @flip_coins.error
     @draw_cards.error
     @bubblewrap.error
+    @hangman.error
     async def error(self, ctx, error):
         if (
             str(error)
@@ -344,6 +429,11 @@ In content: Must be 2000 or fewer in length."""
             await ctx.send(
                 "Sorry, you specified numbers that were too large. Sending all that would put me over the 2000-character limit!"
             )
+        elif (
+            str(error)
+            == "Command raised an exception: HTTPException: 400 Bad Request (error code: 50006): Cannot send an empty message"
+        ):
+            pass
         else:
             await ctx.send(
                 f"Unhandled error occurred:\n```{error}```\nIf my developer (<@!686984544930365440>) is not here, please tell him what the error is so that he can add handling or fix the issue!"
--- pengaelicutils.py
+++ pengaelicutils.py
@@ -309,6 +309,60 @@ syllables = [
     "zu",
 ]
 
+hangman_words = {
+    "pengaelic": "The Pengaelics are a humanoid species that live in the Caretaker's Garden. They're doomed to go extinct, even more so after their planet was destroyed, but that won't stop them from trying to live the best lives they can.",
+    "endron": "The Endrons are a curious humanoid species, they can perform extraordinary feats of magic, and it is said that they are descended from long-lost gods. Their magic is directly tied to the amount of mass on their bodies, converting the mass into magic energy like a sort of fuel.",
+    "symbiote": "A peculiar thing about the Endrons... Inside their stomachs, there is no acid, instead there are little sentient slimes that they have built a symbiotic relationship with. They can tell the difference between friend and food, though there is influence from their hosts as to what they should digest or not.",
+    "xikolarian": "The Xikolarians are a violent warrior species with an ungodly amount of arms. The more arms you have, the higher you are in society. They have created incredible technology and have gone so far as to create vast mechanical fortresses to protect against invaders.",
+    "kragonian": "Not much is known about the Kragonians. They are all identical at birth, differentiating each other with colored paint.",
+    "giblof": "The Giblof are a colossal humanoid species, though you could say that they're mostly gentle giants. They have an unusual internal anatomy, most notably two parallel stomachs on either side of the torso.",
+    "pengaelus": "Pengaelus was a small planet, only a little bigger than Earth's moon. A species known as the Pengaelics came to exist there, almost entirely by accident! In 1995, the Pioneer 11 lost communication with Earth because it was pulled into some freak warp accident and transported an entire lightyear away in mere moments. It crashed on Pengaelus, and the traces of human DNA jumpstarted evolution at an astronomical rate.",
+    "endrolus": "Endrolus is an average-sized planet where the Endrons call home. There's beautiful scenery everywhere, all under a pale violet sky.",
+    "catamunara": "Catamunara is a slightly smaller planet near Endrolus, but there's a secret about it. The planet actually used to be an Endron, who was also named Catamunara. And now people live on her, collecting resources grown on the rich soil around her. Watch out for the subterranean ocean though, because it's alive.",
+    "xikolara": "Xikolara is a huge planet where the Xikolarians reside. The Xikolarians inadvertently made the climate very harsh, so there aren't many small towns on it anymore, the population is concentrated into a handful of huge cities. The surface of the planet is covered in sprawling machinery created by the Xikolarians, which are what affected the climate in the first place, creating lots of greenhouse gases.",
+    "kragon": "Kragon is a fairly average planet. Like its inhabitants, not much is known about it.",
+    "giblona": "Giblona is a planet that was unfortunately lost to a great nuclear winter, and the Giblof have left to wander the galaxy in their Starcity. The gods are working to fix the planet, but it's taking a long time...",
+    "garden": "Somewhere between the universes, there will almost always be a Garden hidden away somewhere. Usually they seem fairly ordinary compared to what you would see on any Class M planet like Earth, but sometimes they are very special, very special indeed.",
+    "tree": "In every Garden, there is the Mother Tree. She holds the Garden together, fills it with magic, and makes sure it can heal itself.",
+    "caretaker": "The Caretaker is the primary person you'll see tending to the Garden. A Caretaker usually has a tall, plump body and a beautiful face, and it is in her nature to be very motherly to any visitors she may have.",
+    "starweaver": "The Starweaver was created with the omniverse, and she will die with it as she creates the next. She can create a silk not unlike a spider's, and she uses it to make sure the stars stay in a stable condition and weave new stars into existence.",
+    "artist": "The Artist is one of the Starweaver's daughters, and she created everything other than the stars with her pencil. Whatever she draws, it comes into existence right in front of her.",
+    "eraser": "The Eraser is the Starweaver's other daughter, and her job is to keep the Artist in check, keep the balance between creation and destruction.",
+    "beloved": "The Beloved is the fertility goddess ruler of Endrolus. She embodies life and its creation, and she is truly beautiful.",
+    "legendmaker": "The Legendmaker is the Beloved's brother, the god who guides lost souls to the Other Side. He wouldn't describe his job as particularly fun, but rather satisfying, knowing that the lost souls have found peace with his guidance.",
+    "judge": "The Judge is the high balance goddess, making sure everything is as it should be, nothing too far above its opposite.",
+    "resonant": "The Resonant is a young goddess, altering the thoughts and emotions of all who hear her music. She almost constantly emanates music from her body, which changes depending on her state of mind.",
+}
+
+regional_indicators = {
+    "🇦": "a",
+    "🇧": "b",
+    "🇨": "c",
+    "🇩": "d",
+    "🇪": "e",
+    "🇫": "f",
+    "🇬": "g",
+    "🇭": "h",
+    "🇮": "i",
+    "🇯": "j",
+    "🇰": "k",
+    "🇱": "l",
+    "🇲": "m",
+    "🇳": "n",
+    "🇴": "o",
+    "🇵": "p",
+    "🇶": "q",
+    "🇷": "r",
+    "🇸": "s",
+    "🇹": "t",
+    "🇺": "u",
+    "🇻": "v",
+    "🇼": "w",
+    "🇽": "x",
+    "🇾": "y",
+    "🇿": "z",
+}
+
 
 def jsoncheck(guild: str):
     return getops(guild, "toggles", "jsonMenus")
