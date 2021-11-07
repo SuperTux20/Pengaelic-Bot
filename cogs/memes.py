d5c9ad0763a6e43567472ba1341e003d1f60ff29
SuperTux20 <tuxpengwin@gmail.com>
2021-11-06 19:44:22 -0700

    damn, these memes don't want to cooperate

--- cogs/memes.py
+++ cogs/memes.py
@@ -25,25 +25,21 @@ class Memes(commands.Cog):
 
 	def getImage(self, ctx) -> Image:
 		if ctx.message.attachments:
-			if self.valid_image(ctx.message.attachments[0].filename):
-				return Image.open(BytesIO(get(ctx.message.attachments[0].url).content))
-			else:
-				return Image.open("images/meme_templates/generic.jpg")
-		else:
-			return Image.open("images/meme_templates/generic.jpg")
+			if self.valid_image(ctx.message.attachments[0].filename):	return Image.open(BytesIO(get(ctx.message.attachments[0].url).content))
+			else:	return Image.open("images/meme_templates/generic.jpg")
 
-	@commands.group(name="2012meme", help="Make a classic top-text bottom-text meme. Supply your own PNG or JPG!", usage="<top text> | [bottom text]")
+		else:	return Image.open("images/meme_templates/generic.jpg")
+
+	@commands.command(name="2012meme", help="Make a classic top-text bottom-text meme. Supply your own PNG or JPG!", usage="<top text> | [bottom text]")
 	async def topbottom(self, ctx, *, text):
 		img	= self.getImage(ctx)
 		width, height	= img.size
 		try:	topString, bottomString = text.upper().split("|")
 		except ValueError as error:
-			if "not enough" in str(error):
-				topString, bottomString	= [text.upper(), ""]
-			else:
-				raise
-		# find biggest font size that works
-		fontSize	= int(height / 5)
+			if "not enough" in str(error):	topString, bottomString	= [text.upper(), ""]
+			else:	raise
+
+		fontSize	= int(height / 5)	# find biggest font size that works
 		font	= ImageFont.truetype("fonts/impact.ttf", fontSize)
 		topTextSize	= font.getsize(topString)
 		bottomTextSize	= font.getsize(bottomString)
@@ -53,8 +49,8 @@ class Memes(commands.Cog):
 			topTextSize	= font.getsize(topString)
 			bottomTextSize	= font.getsize(bottomString)
 
-		topTextPosition	= ((width / 2) - (topTextSize[0] / 2), 0)
-		bottomTextPosition	= ((width / 2) - (bottomTextSize[0] / 2), height - bottomTextSize[1])
+		topTextPosition	= ((width / 2) - (topTextSize[0]	/ 2), 0)
+		bottomTextPosition	= ((width / 2) - (bottomTextSize[0]	/ 2), height - bottomTextSize[1])
 		draw	= ImageDraw.Draw(img)
 		outlineRange	= int(fontSize / 15)	# draw outlines
 		for x in range(-outlineRange, outlineRange + 1):
@@ -66,13 +62,12 @@ class Memes(commands.Cog):
 		draw.text(bottomTextPosition,	bottomString,	0xffffffff, font=font)
 		await ctx.send(file=self.Image2File(img, "2012meme.png"))
 
-	@commands.group(name="2016meme", help="Make a more modern top-text meme. Supply your own PNG or JPG!", usage="<text> | [text] | [text] | [etc]")
+	@commands.command(name="2016meme", help="Make a more modern top-text meme. Supply your own PNG or JPG!", usage="<text> | [text] | [text] | [etc]")
 	async def toponly(self, ctx, *, text):
 		img	= self.getImage(ctx)
 		width, height	= img.size
 		text	= text.split("|")
-		# find biggest font size that works
-		fontSize	= int(height / 10)
+		fontSize	= int(height / 10)	# find biggest font size that works
 		font	= ImageFont.truetype("fonts/liberation.ttf", fontSize)
 		for line in text:
 			textSize	= font.getsize(line)
@@ -88,8 +83,77 @@ class Memes(commands.Cog):
 		for line in text:	draw.text((0, (fontSize * text.index(line))),	line,	(0, 0, 0), font=font)
 		await ctx.send(file=self.Image2File(pad, "2016meme.png"))
 
+	# FIXME: holy fuck come back to this later, text issues
+	# @commands.command(name="drakememe", help="Make a drake meme.", usage="<text> | <text>")
+	# async def drake(self, ctx, *, text):
+	#	img	= Image.open("images/meme_templates/drake.jpg")
+	#	width, height	= [img.size[0]*2, img.size[1]]
+	#	try:	topString, bottomString = text.split("|")
+	#	except ValueError as error:
+	#		if "not enough" in str(error):	topString, bottomString	= [text, ""]
+	#		else:	raise
+
+	#	fontSize	= int(height / 10)	# find biggest font size that works
+	#	font	= ImageFont.truetype("fonts/liberation.ttf", fontSize)
+	#	topTextSize	= font.getsize(topString)
+	#	bottomTextSize	= font.getsize(bottomString)
+	#	twrap = 0
+	#	bwrap = 0
+	#	while topTextSize[0] > width - 20 or bottomTextSize[0] > width - 20:
+	#		tmp = topString.split()
+	#		if len(topString.split("\n")) > 1:	tmp += [topString.split("\n")[1]]
+	#		try:	topString = " ".join(tmp[:-twrap]) + "\n".join(tmp[-twrap:])
+	#		except IndexError:	pass
+	#		twrap += 1
+	#		tmp = bottomString.split()
+	#		if len(bottomString.split("\n")) > 1:	tmp += [bottomString.split("\n")[1]]
+	#		try:	bottomString = " ".join(tmp[:-bwrap]) + "\n".join(tmp[-bwrap:])
+	#		except IndexError:	pass
+	#		bwrap += 1
+	#		try:	topTextSize	= (font.getsize(topString)[0] - font.getsize("\n".join(topString.split()[-twrap:]))[0], font.getsize(topString)[1] - font.getsize("\n".join(topString.split()[-twrap:]))[1])
+	#		except IndexError:	topTextSize	= font.getsize(topString)
+	#		try:	bottomTextSize	= (font.getsize(bottomString)[0] - font.getsize("\n".join(bottomString.split()[-bwrap:]))[0], font.getsize(bottomString)[1] - font.getsize("\n".join(bottomString.split()[-bwrap:]))[1])
+	#		except IndexError:	bottomTextSize	= font.getsize(bottomString)
+
+	#	pad = Image.new(img.mode, (width, height), 0xffffffff)	# add white padding
+	#	pad.paste(img, (0, 0))
+	#	topTextPosition	= (((width / 2) - (topTextSize[0]	/ 2)), 10)
+	#	bottomTextPosition	= (((width / 2) - (bottomTextSize[0]	/ 2)), bottomTextSize[1] + 500)
+	#	if "\n" in topString:	topTextPosition = (topTextPosition[0] + width / 2, topTextPosition[1])
+	#	if "\n" in bottomString:	bottomTextPosition = ((bottomTextPosition[0] + width / 2) - width / 24, bottomTextPosition[1] + width / 12)
+
+	#	draw	= ImageDraw.Draw(pad)
+
+	#	draw.text(topTextPosition,	topString,	(0, 0, 0), font=font)
+	#	draw.text(bottomTextPosition,	bottomString,	(0, 0, 0), font=font)
+	#	await ctx.send(file=self.Image2File(pad, "drakememe.png"))
+
+	# TODO: actually make this meme
+	# @commands.command(name="galaxybrainmeme", help="Smarter and smarter.", usage="<text> | [text] | [text] | [etc]")
+	# async def galaxybrain(self, ctx, *, text):
+	#	img	= Image.open("images/meme_templates/galaxy_brain.jpg")
+	#	width, height	= img.size
+	#	text	= text.split("|")
+	#	fontSize	= int(height / 10)	# find biggest font size that works
+	#	font	= ImageFont.truetype("fonts/liberation.ttf", fontSize)
+	#	for line in text:
+	#		textSize	= font.getsize(line)
+	#		while textSize[0] > width - 20:
+	#			fontSize	= fontSize - 1
+	#			font	= ImageFont.truetype("fonts/liberation.ttf", fontSize)
+	#			textSize	= font.getsize(line)
+
+	#	pad = Image.new(img.mode, (width, height + (fontSize * len(text))), 0xffffffff)	# add white padding
+	#	pad.paste(img, (0, (fontSize * len(text)) + 10))
+	#	draw	= ImageDraw.Draw(pad)
+
+	#	for line in text:	draw.text((0, (fontSize * text.index(line))),	line,	(0, 0, 0), font=font)
+	#	await ctx.send(file=self.Image2File(pad, "galaxybrainmeme.png"))
+
 	@topbottom.error
 	@toponly.error
+	# @drake.error
+	# @galaxybrain.error
 	async def messageError(self, ctx, error):
 		error = str(error)
 		if "text is a required argument that is missing." in error:	await ctx.send("<:winxp_warning:869760947114348604>No caption specified!")
