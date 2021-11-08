d583a7db4c344293fffeb831069b0b4a9e890fa8
SuperTux20 <tuxpengwin@gmail.com>
2021-11-08 09:35:44 -0800

    rename variables and add typing indicator

--- cogs/memes.py
+++ cogs/memes.py
@@ -2,6 +2,8 @@
 # -*- coding: utf-8 -*-
 
 import	discord
+from asyncio.events import	get_event_loop
+from concurrent.futures import	ThreadPoolExecutor
 from io import	BytesIO
 from discord.ext import	commands
 from pengaelicutils import	jsoncheck,	unhandling,	tux_in_guild
@@ -30,102 +32,109 @@ class Memes(commands.Cog):
 
 		else:	return Image.open("images/meme_templates/generic.jpg")
 
-	@commands.command(name="2012meme", help="Make a classic top-text bottom-text meme. Supply your own PNG or JPG!", usage="<top text> | [bottom text]")
-	async def topbottom(self, ctx, *, text):
-		img	= self.getImage(ctx)
+	def twelve(self, img: Image, text: str) -> Image:
 		width, height	= img.size
-		try:	topString, bottomString = text.upper().split("|")
+		try:	top_string, bottom_string = text.upper().split("|")
 		except ValueError as error:
-			if "not enough" in str(error):	topString, bottomString	= [text.upper(), ""]
+			if "not enough" in str(error):	top_string, bottom_string	= [text.upper(), ""]
 			else:	raise
 
-		fontSize	= int(height / 5)	# find biggest font size that works
-		font	= ImageFont.truetype("fonts/impact.ttf", fontSize)
-		topTextSize	= font.getsize(topString)
-		bottomTextSize	= font.getsize(bottomString)
-		while topTextSize[0] > width - 20 or bottomTextSize[0] > width - 20:
-			fontSize	= fontSize - 1
-			font	= ImageFont.truetype("fonts/impact.ttf", fontSize)
-			topTextSize	= font.getsize(topString)
-			bottomTextSize	= font.getsize(bottomString)
+		font_size	= int(height / 5)	# find biggest font size that works
+		font	= ImageFont.truetype("fonts/impact.ttf", font_size)
+		top_text_size	= font.getsize(top_string)
+		bottom_text_size	= font.getsize(bottom_string)
+		while top_text_size[0] > width - 20 or bottom_text_size[0] > width - 20:
+			font_size	= font_size - 1
+			font	= ImageFont.truetype("fonts/impact.ttf", font_size)
+			top_text_size	= font.getsize(top_string)
+			bottom_text_size	= font.getsize(bottom_string)
 
-		topTextPosition	= ((width / 2) - (topTextSize[0]	/ 2), 0)
-		bottomTextPosition	= ((width / 2) - (bottomTextSize[0]	/ 2), height - bottomTextSize[1])
+		top_text_pos	= ((width / 2) - (top_text_size[0]	/ 2), 0)
+		bottom_text_pos	= ((width / 2) - (bottom_text_size[0]	/ 2), height - bottom_text_size[1])
 		draw	= ImageDraw.Draw(img)
-		outlineRange	= int(fontSize / 15)	# draw outlines
-		for x in range(-outlineRange, outlineRange + 1):
-			for y in range(-outlineRange, outlineRange + 1):
-				draw.text((topTextPosition[0] + x,	topTextPosition[1] + y),	topString,	(0, 0, 0), font=font)
-				draw.text((bottomTextPosition[0] + x,	bottomTextPosition[1] + y),	bottomString,	(0, 0, 0), font=font)
+		outline_range	= int(font_size / 15)	# draw outlines
+		for x in range(-outline_range, outline_range + 1):
+			for y in range(-outline_range, outline_range + 1):
+				draw.text((top_text_pos[0] + x,	top_text_pos[1] + y),	top_string,	(0, 0, 0), font=font)
+				draw.text((bottom_text_pos[0] + x,	bottom_text_pos[1] + y),	bottom_string,	(0, 0, 0), font=font)
 
-		draw.text(topTextPosition,	topString,	0xffffffff, font=font)
-		draw.text(bottomTextPosition,	bottomString,	0xffffffff, font=font)
-		await ctx.send(file=self.Image2File(img, "2012meme.png"))
+		draw.text(top_text_pos,	top_string,	0xffffffff, font=font)
+		draw.text(bottom_text_pos,	bottom_string,	0xffffffff, font=font)
+
+		return img
+
+	def sixteen(self, img: Image, text: str) -> Image:
+		width, height	= img.size
+		text	= text.split("|")
+		font_size	= int(height / 10)	# find biggest font size that works
+		font	= ImageFont.truetype("fonts/liberation.ttf", font_size)
+		for line in text:
+			text_size	= font.getsize(line)
+			while text_size[0] > width - 20:
+				font_size	= font_size - 1
+				font	= ImageFont.truetype("fonts/liberation.ttf", font_size)
+				text_size	= font.getsize(line)
+
+		pad = Image.new(img.mode, (width, height + (font_size * len(text))), 0xffffffff)	# add white padding
+		pad.paste(img, (0, (font_size * len(text)) + 10))
+		draw	= ImageDraw.Draw(pad)
+
+		for line in text:	draw.text((0, (font_size * text.index(line))),	line,	(0, 0, 0), font=font)
+		return pad
+
+	@commands.command(name="2012meme", help="Make a classic top-text bottom-text meme. Supply your own PNG or JPG!", usage="<top text> | [bottom text]")
+	async def topbottom(self, ctx, *, text):
+		async with ctx.typing():	img = await get_event_loop().run_in_executor(ThreadPoolExecutor(), self.Image2File, self.twelve(self.getImage(ctx), text), "2012meme.png")
+		await ctx.send(file=img)
 
 	@commands.command(name="2016meme", help="Make a more modern top-text meme. Supply your own PNG or JPG!", usage="<text> | [text] | [text] | [etc]")
 	async def toponly(self, ctx, *, text):
-		img	= self.getImage(ctx)
-		width, height	= img.size
-		text	= text.split("|")
-		fontSize	= int(height / 10)	# find biggest font size that works
-		font	= ImageFont.truetype("fonts/liberation.ttf", fontSize)
-		for line in text:
-			textSize	= font.getsize(line)
-			while textSize[0] > width - 20:
-				fontSize	= fontSize - 1
-				font	= ImageFont.truetype("fonts/liberation.ttf", fontSize)
-				textSize	= font.getsize(line)
-
-		pad = Image.new(img.mode, (width, height + (fontSize * len(text))), 0xffffffff)	# add white padding
-		pad.paste(img, (0, (fontSize * len(text)) + 10))
-		draw	= ImageDraw.Draw(pad)
-
-		for line in text:	draw.text((0, (fontSize * text.index(line))),	line,	(0, 0, 0), font=font)
-		await ctx.send(file=self.Image2File(pad, "2016meme.png"))
+		async with ctx.typing():	img = await get_event_loop().run_in_executor(ThreadPoolExecutor(), self.Image2File, self.sixteen(self.getImage(ctx), text), "2016meme.png")
+		await ctx.send(file=img)
 
 	# FIXME: holy fuck come back to this later, text issues
 	# @commands.command(name="drakememe", help="Make a drake meme.", usage="<text> | <text>")
 	# async def drake(self, ctx, *, text):
 	#	img	= Image.open("images/meme_templates/drake.jpg")
 	#	width, height	= [img.size[0]*2, img.size[1]]
-	#	try:	topString, bottomString = text.split("|")
+	#	try:	top_string, bottom_string = text.split("|")
 	#	except ValueError as error:
-	#		if "not enough" in str(error):	topString, bottomString	= [text, ""]
+	#		if "not enough" in str(error):	top_string, bottom_string	= [text, ""]
 	#		else:	raise
 
-	#	fontSize	= int(height / 10)	# find biggest font size that works
-	#	font	= ImageFont.truetype("fonts/liberation.ttf", fontSize)
-	#	topTextSize	= font.getsize(topString)
-	#	bottomTextSize	= font.getsize(bottomString)
-	#	twrap = 0
-	#	bwrap = 0
-	#	while topTextSize[0] > width - 20 or bottomTextSize[0] > width - 20:
-	#		tmp = topString.split()
-	#		if len(topString.split("\n")) > 1:	tmp += [topString.split("\n")[1]]
-	#		try:	topString = " ".join(tmp[:-twrap]) + "\n".join(tmp[-twrap:])
+	#	font_size	= int(height / 10)	# find biggest font size that works
+	#	font	= ImageFont.truetype("fonts/liberation.ttf", font_size)
+	#	top_text_size	= font.getsize(top_string)
+	#	bottom_text_size	= font.getsize(bottom_string)
+	#	top_wrap = 0
+	#	bottom_wrap = 0
+	#	while top_text_size[0] > width - 20 or bottom_text_size[0] > width - 20:
+	#		tmp = top_string.split()
+	#		if len(top_string.split("\n")) > 1:	tmp += [top_string.split("\n")[1]]
+	#		try:	top_string = " ".join(tmp[:-top_wrap]) + "\n".join(tmp[-top_wrap:])
 	#		except IndexError:	pass
-	#		twrap += 1
-	#		tmp = bottomString.split()
-	#		if len(bottomString.split("\n")) > 1:	tmp += [bottomString.split("\n")[1]]
-	#		try:	bottomString = " ".join(tmp[:-bwrap]) + "\n".join(tmp[-bwrap:])
+	#		top_wrap += 1
+	#		tmp = bottom_string.split()
+	#		if len(bottom_string.split("\n")) > 1:	tmp += [bottom_string.split("\n")[1]]
+	#		try:	bottom_string = " ".join(tmp[:-bottom_wrap]) + "\n".join(tmp[-bottom_wrap:])
 	#		except IndexError:	pass
-	#		bwrap += 1
-	#		try:	topTextSize	= (font.getsize(topString)[0] - font.getsize("\n".join(topString.split()[-twrap:]))[0], font.getsize(topString)[1] - font.getsize("\n".join(topString.split()[-twrap:]))[1])
-	#		except IndexError:	topTextSize	= font.getsize(topString)
-	#		try:	bottomTextSize	= (font.getsize(bottomString)[0] - font.getsize("\n".join(bottomString.split()[-bwrap:]))[0], font.getsize(bottomString)[1] - font.getsize("\n".join(bottomString.split()[-bwrap:]))[1])
-	#		except IndexError:	bottomTextSize	= font.getsize(bottomString)
+	#		bottom_wrap += 1
+	#		try:	top_text_size	= (font.getsize(top_string)[0] - font.getsize("\n".join(top_string.split()[-top_wrap:]))[0], font.getsize(top_string)[1] - font.getsize("\n".join(top_string.split()[-top_wrap:]))[1])
+	#		except IndexError:	top_text_size	= font.getsize(top_string)
+	#		try:	bottom_text_size	= (font.getsize(bottom_string)[0] - font.getsize("\n".join(bottom_string.split()[-bottom_wrap:]))[0], font.getsize(bottom_string)[1] - font.getsize("\n".join(bottom_string.split()[-bottom_wrap:]))[1])
+	#		except IndexError:	bottom_text_size	= font.getsize(bottom_string)
 
 	#	pad = Image.new(img.mode, (width, height), 0xffffffff)	# add white padding
 	#	pad.paste(img, (0, 0))
-	#	topTextPosition	= (((width / 2) - (topTextSize[0]	/ 2)), 10)
-	#	bottomTextPosition	= (((width / 2) - (bottomTextSize[0]	/ 2)), bottomTextSize[1] + 500)
-	#	if "\n" in topString:	topTextPosition = (topTextPosition[0] + width / 2, topTextPosition[1])
-	#	if "\n" in bottomString:	bottomTextPosition = ((bottomTextPosition[0] + width / 2) - width / 24, bottomTextPosition[1] + width / 12)
+	#	top_text_pos	= (((width / 2) - (top_text_size[0]	/ 2)), 10)
+	#	bottom_text_pos	= (((width / 2) - (bottom_text_size[0]	/ 2)), bottom_text_size[1] + 500)
+	#	if "\n" in top_string:	top_text_pos = (top_text_pos[0] + width / 2, top_text_pos[1])
+	#	if "\n" in bottom_string:	bottom_text_pos = ((bottom_text_pos[0] + width / 2) - width / 24, bottom_text_pos[1] + width / 12)
 
 	#	draw	= ImageDraw.Draw(pad)
 
-	#	draw.text(topTextPosition,	topString,	(0, 0, 0), font=font)
-	#	draw.text(bottomTextPosition,	bottomString,	(0, 0, 0), font=font)
+	#	draw.text(top_text_pos,	top_string,	(0, 0, 0), font=font)
+	#	draw.text(bottom_text_pos,	bottom_string,	(0, 0, 0), font=font)
 	#	await ctx.send(file=self.Image2File(pad, "drakememe.png"))
 
 	# TODO: actually make this meme
@@ -134,20 +143,20 @@ class Memes(commands.Cog):
 	#	img	= Image.open("images/meme_templates/galaxy_brain.jpg")
 	#	width, height	= img.size
 	#	text	= text.split("|")
-	#	fontSize	= int(height / 10)	# find biggest font size that works
-	#	font	= ImageFont.truetype("fonts/liberation.ttf", fontSize)
+	#	font_size	= int(height / 10)	# find biggest font size that works
+	#	font	= ImageFont.truetype("fonts/liberation.ttf", font_size)
 	#	for line in text:
-	#		textSize	= font.getsize(line)
-	#		while textSize[0] > width - 20:
-	#			fontSize	= fontSize - 1
-	#			font	= ImageFont.truetype("fonts/liberation.ttf", fontSize)
-	#			textSize	= font.getsize(line)
+	#		text_size	= font.getsize(line)
+	#		while text_size[0] > width - 20:
+	#			font_size	= font_size - 1
+	#			font	= ImageFont.truetype("fonts/liberation.ttf", font_size)
+	#			text_size	= font.getsize(line)
 
-	#	pad = Image.new(img.mode, (width, height + (fontSize * len(text))), 0xffffffff)	# add white padding
-	#	pad.paste(img, (0, (fontSize * len(text)) + 10))
+	#	pad = Image.new(img.mode, (width, height + (font_size * len(text))), 0xffffffff)	# add white padding
+	#	pad.paste(img, (0, (font_size * len(text)) + 10))
 	#	draw	= ImageDraw.Draw(pad)
 
-	#	for line in text:	draw.text((0, (fontSize * text.index(line))),	line,	(0, 0, 0), font=font)
+	#	for line in text:	draw.text((0, (font_size * text.index(line))),	line,	(0, 0, 0), font=font)
 	#	await ctx.send(file=self.Image2File(pad, "galaxybrainmeme.png"))
 
 	@topbottom.error
