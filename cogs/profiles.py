3774656334d78334b8845f1ff56fd90e3e46f6f3
SuperTux20 <tuxpengwin@gmail.com>
2021-11-09 19:20:09 -0800

    add birthday and sexuality

--- cogs/profiles.py
+++ cogs/profiles.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3.9
 # -*- coding: utf-8 -*-
 
-import	discord
-from discord.ext import	commands
-from json import	dumps
-from tinydb import	TinyDB,	Query
-from pengaelicutils import	jsoncheck,	unhandling,	tux_in_guild,	Developers
+from discord	import Embed
+from datetime	import datetime
+from discord.ext	import commands
+from json	import dumps
+from tinydb	import TinyDB,	Query
+from pengaelicutils	import unhandling,	tux_in_guild, img2file, url2img
+from PIL	import Image,	ImageFont,	ImageDraw
 
 class Profiles(commands.Cog):
 	def __init__(self, client):	self.client	= client
@@ -18,60 +20,96 @@ class Profiles(commands.Cog):
 
 	# SECTION FUNCTIONS
 	# ANCHOR: GENERATE NEW PROFILE
-	newprof = lambda self: {
-		"bio":	None,
-		"image":	None,
-		"motd":	None,
-		"nickname":	None,
-		"pronouns":	[],
-		"region":	None,
+	newprof = lambda self: {key: None for key in [
+			"bio",
+			"birthday",
+			"image",
+			"motd",
+			"nickname",
+			"pronouns",
+			"region",
+			"sexuality"
+		]
 	}
-
 
 	# ANCHOR: GET PROFILE
 	def getprof(self, member: str) -> dict:
 		user	= Query()
 		profile	= dict(sorted(self.db.search(user.userID == member)[0].items()))
-		profile.pop("userName")
-		profile.pop("userID")
-		return profile
-
+		[profile.pop(key) for key in ["userName", "userID", "image"]]
+		if len(profile["region"]) == 2:	profile["region"] = f"flag_{profile['region']}"
+		profile["region"] = f":{profile['region']}:"
+		return dumps(profile, indent=0)[1:-2].replace('"',"").replace(",","").replace("null", "No value set.")
 
 	# ANCHOR: UPDATE PROFILE
-	def uprof(self, member: str, category: str, option: str, value):
+	async def uprof(self, ctx, cond, option: str, value):
 		user	= Query()
-		options	= self.db.search(user.userID == member)[0][category]
-		options[option]	= value
-		self.db.update({category: options}, user.userID == member)
+		profile	= self.db.search(user.userID == ctx.author.id)[0]
+		profile[option]	= value
+		self.db.update(profile, user.userID == ctx.author.id)
+		await ctx.send(f"<:winxp_information:869760946808180747>{'Updated' if cond else 'Removed'} {option}.")
 	# END SECTION
 
-	@commands.group(name="profile", help="Take a look at your profile!")
-	async def showprofile(self, ctx):
+	@commands.group(name="profile", help="Take a look at your profile!", aliases=["me"])
+	async def profile(self, ctx):
 		if ctx.invoked_subcommand == None:
 			try:
-				# add any options that may have been created since the profile's creation
+				# add any parameters that may have been created since the profile's creation
 				user	= Query()
 				uid	= ctx.author.id
 				nof	= self.newprof()
 				prof	= self.db.search(user.userID == uid)[0]
-				prof.pop("userName")
-				prof.pop("userID")
-				for rof in ["image", "nickname", "bio", "motd", "region", "pronouns"]:
+				[prof.pop(key) for key in ["userName", "userID"]]
+				for rof in list(nof.keys()):
 					if rof not in nof:	prof.pop(rof)
-				for rof in ["image", "nickname", "bio", "motd", "region", "pronouns"]:
+				for rof in list(nof.keys()):
 					prof = dict(list(nof.items()) + list(prof.items()))
 					self.db.update(dict(sorted(prof.items())), user.userID == uid)
 
 				self.db.update({"userName": ctx.author.name}, user.userID == uid)	# did the user's name change?
 				await ctx.send(f"Profile for {ctx.author.name}")
-				await ctx.send(f"```json\n{dumps(self.getprof(uid), indent=4)}\n```")
+				await ctx.send(self.getprof(ctx.author.id))
 			except IndexError:
 				newprofile	= {"userName": ctx.author.name, "userID": ctx.author.id} | self.newprof()
 				self.db.insert(newprofile)
 				await ctx.send(f"Created new profile for {ctx.author.name}")
-				await ctx.send(f"```json\n{dumps(self.getprof(ctx.author.id), indent=4)}\n```")
+				await ctx.send(self.getprof(ctx.author.id))
 
-	@showprofile.error
+	@profile.command(name="help", help="Get help on how to configure your profile.", aliases=["commands", "?"])
+	async def set_bio(self, ctx, *, text=None): await ctx.send(embed=Embed(title="Profiles"))
+
+	@profile.command(name="bio", help="Set a bio for your profile.", aliases=["about", "aboutme"])
+	async def set_bio(self, ctx, *, text=None): await self.uprof(ctx, text, "bio", text)
+
+	@profile.command(name="birthday", help="Set a birthday for your profile.", aliases=["bday", "bd"], usage="<M>/<D>/[YYYY]")
+	async def set_bday(self, ctx, *, text=None): await self.uprof(ctx, text, "birthday", datetime.strftime(datetime.strptime(text, "%m/%d/%Y"), "%B %-d %Y"))
+
+	@profile.command(name="image", help="Set an image for your profile with attachment or URL.", aliases=["img", "background", "bg"])
+	async def set_img(self, ctx, text=None): await self.uprof(ctx, text or ctx.message.attachments, "image", ctx.message.attachments[0].url if ctx.message.attachments else text)
+
+	@profile.command(name="motd", help='Set a "Message of the Day"/motto/slogan for your profile.', aliases=["motto", "slogan", "tagline"])
+	async def set_motd(self, ctx, *, text=None): await self.uprof(ctx, text, "motd", text)
+
+	@profile.command(name="nickname", help="Set a nickname for your profile.", aliases=["nick", "name"])
+	async def set_nick(self, ctx, *, text=None): await self.uprof(ctx, text, "nickname", text)
+
+	@profile.command(name="pronouns", help="Set pronouns for your profile.")
+	async def set_pronouns(self, ctx, text=None): await self.uprof(ctx, text, "pronouns", text)
+
+	@profile.command(name="region", help="Set a region for your profile.", aliases=["country"])
+	async def set_region(self, ctx, text=None): await self.uprof(ctx, text, "region", text)
+
+	@profile.command(name="sexuality", help="Set a sexuality for your profile.")
+	async def set_sexuality(self, ctx, text=None): await self.uprof(ctx, text, "sexuality", text)
+
+	@profile.error
+	@set_bio.error
+	@set_img.error
+	@set_motd.error
+	@set_nick.error
+	@set_pronouns.error
+	@set_region.error
+	@set_sexuality.error
 	async def error(self, ctx, error):
 		error = str(error)
 		await ctx.send(unhandling(error, tux_in_guild(ctx, self.client)))
